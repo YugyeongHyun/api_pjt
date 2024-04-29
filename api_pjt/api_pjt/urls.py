df1b76d1332b3552d96e4e9422a95c7aefc2a40d
jessie.hyun <jessie.hyun@yorksj.ac.uk>
2024-04-29 17:57:12 +0900

    crud 구현

--- api_pjt/api_pjt/urls.py
+++ api_pjt/api_pjt/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/articles', include('articles.urls')),
+    path('api/v1/articles/', include('articles.urls')),
 ]
--- api_pjt/articles/views.py
+++ api_pjt/articles/views.py
@@ -1,14 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, JsonResponse
 from .models import Article
 from django.core import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializer import ArticleSerializer
+from rest_framework import status
 
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def article_list(request):
-    articles = Article.objects.all()
-    serializer = ArticleSerializer(articles, many=True)
-    return Response(serializer.data)
+    if request.method == "GET":
+        articles = Article.objects.all()
+        serializer = ArticleSerializer(articles, many=True)
+        return Response(serializer.data)
+
+    elif request.method == "POST":
+        serializer = ArticleSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@ api_view(['GET', 'PUT', 'DELETE'])
+def article_detail(request, pk):
+    if request.method == "GET":
+        article = get_object_or_404(Article, pk=pk)
+        serializer = ArticleSerializer(article)
+        return Response(serializer.data)
+
+    elif request.method == "PUT":
+        article = get_object_or_404(Article, pk=pk)
+        serializer = ArticleSerializer(
+            article, data=request.data, partial=True)
+        if serializer.is_valid(raise_exception=True):
+            serializer.save()
+            return Response(serializer.data)
+
+    elif request.method == "DELETE":
+        article = get_object_or_404(Article, pk=pk)
+        article.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
